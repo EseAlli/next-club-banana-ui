c14f5af2ed12609217841f975cedfeaac0f7f921
EseAlli <essealli.ea@gmail.com>
2022-08-10 21:14:41 +0100

    removed moment

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import moment from 'moment';
+import styles from '../styles/Home.module.css';
 import {useState} from "react"
 import Image from 'next/image';
 
 import Footer from '../components/Footer';
 import {apps} from '../utils/index'
-
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 export default function Home() {
   const [lock, setLock] = useState(true)
-  let time = moment().format("h:mm")
-  let date = moment().format("dddd, MMMM D")
+  const d = new Date();
+  const time = d.getHours() + ":" + d.getMinutes() 
+  let day = days[d.getDay()];
+  let month = months[d.getMonth()];
 
   const unlock = () =>{
     if(lock){
@@ -29,7 +31,7 @@ export default function Home() {
       <div className={styles.homepage} onClick={()=>unlock()}>
           <div className={lock ? styles.lockscreen : `${styles.lockscreen}  ${styles.visibility}`}> 
               <p className={styles.time}>{time}</p>
-              <p className={styles.date}>{date}</p>
+              <p className={styles.date}>{`${day}, ${month} ${d.getDate()}`}</p>
               <p className={styles.unlock}>touch to unlock</p>
           </div>
           {
