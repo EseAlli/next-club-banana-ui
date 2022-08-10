7524b2a21f20fc2aa579659a54be91f63c9c1ed8
EseAlli <essealli.ea@gmail.com>
2022-08-10 20:20:44 +0100

    styling done

---
+++ utils/index.js
@@ -0,0 +1,63 @@
+import apple from '../public/assets/icons/apple_music.webp';
+import facebook from '../public/assets/icons/facebook.webp';
+import instagram from '../public/assets/icons/instagram.webp';
+import clbn from '../public/assets/icons/clbn.jpg';
+import soundcloud from '../public/assets/icons/soundcloud.webp';
+import spotify from '../public/assets/icons/spotify.webp';
+import tidal from '../public/assets/icons/tidal.webp';
+import twitter from '../public/assets/icons/twitter.webp';
+import youtube from '../public/assets/icons/youtube.webp';
+import tumblr from '../public/assets/icons/tumblr.webp';
+
+export const apps = [
+    {
+        name: "Apple Music",
+        image: apple,
+        url: "https://music.apple.com/us/artist/amin%C3%A9/1097595072?v=1660146081260"
+    },
+    {
+        name: "Spotify",
+        image: spotify,
+        url: "https://open.spotify.com/artist/3Gm5F95VdRxW3mqCn8RPBJ?v=1660146081260"
+    },
+    {
+        name: "Soundcloud",
+        image: soundcloud,
+        url: "https://soundcloud.com/heyamine?v=1660146081260"
+    },
+    {
+        name: "Tidal",
+        image: tidal,
+        url: "https://listen.tidal.com/artist/5847194?v=1660146081262"
+    },
+    {
+        name: "Youtube",
+        image: youtube,
+        url: "https://www.youtube.com/Amine?v=1660146081262"
+    },
+    {
+        name: "Twitter",
+        image: twitter,
+        url: "https://twitter.com/heyamine?lang=en?v=1660146081262"
+    },
+    {
+        name: "Instagram",
+        image: instagram,
+        url: "https://www.instagram.com/amine/?v=1660146081263"
+    },
+    {
+        name: "Shop",
+        image: clbn,
+        url: "https://clubbanana.com/shop?v=1660146081263"
+    },
+    {
+        name: "Tumblr",
+        image: tumblr,
+        url: "http://amineknows.tumblr.com/?v=1660146081263"
+    },
+    {
+        name: "Facebook",
+        image: facebook,
+        url: "https://www.facebook.com/heyamine?v=1660146081263"
+    }
+]
